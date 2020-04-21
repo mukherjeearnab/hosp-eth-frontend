f2ea452480333a5e1f3d0240e51ac6bf20e1f22b
mukherjeearnab <mukherjeearnab905@gmail.com>
2020-04-21 20:53:51 +0530

    added removeMod/Doc

--- src/contract.js
+++ src/contract.js
@@ -1,6 +1,6 @@
 import web3 from "./web3";
 
-const address = "0x9a648eaD75aabe78F8571a9f4Cc44Ea26c7C5A2b";
+const address = "0x38BF38541aDa16c983699aDF01b3c971dbfB5301";
 
 const abi = require("./ABI.json");
 
